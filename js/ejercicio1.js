8bc8323e1d721f68c7f1138497d79186c407e911
Federico <federuedaleal@hotmail.com>
2023-03-18 00:04:24 -0300

    Ejercicio 1 resuelto, añadido el h1

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,4 +1,5 @@
 let meses = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre']
+document.write('<h1>Lista de Meses</h1>')
 document.write('<ul>')
 for (let i = 0; i < meses.length; i++){
     document.write(`<li>${meses[i]}</li>`)
---
+++ js/ejercicio2.js
@@ -0,0 +1,14 @@
+let ciudadUser = prompt('Ingrese una ciudad')
+let ciudadesTot = [ciudadUser]
+
+for (let i = 0; i < ciudadUser.length; ciudadUser++) {
+    document.write(`La ciudad ingresada es ${ciudadUser}`)
+    }
+    if (confirmar==true){
+let continuar = confirm('Desea continuar?')
+        document.write('bien hecho')
+    }
+
+//else {
+  //  document.write('hoa')
+//}
